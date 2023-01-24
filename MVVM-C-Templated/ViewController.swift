8597545660b17190a6e4a953ce7deb166a0ae8e7
Arun Sirrpi <sinthanaisirrpi.arun@abc.net.au>
2023-01-24 20:47:19 +1100

    Initial Commit

---
+++ MVVM-C-Templated/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-C-Templated
+//
+//  Created by Arun Sinthanaisirrpi on 24/1/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
